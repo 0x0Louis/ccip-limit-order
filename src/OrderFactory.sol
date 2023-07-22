e7bfca04a0d9ee702303f739bcbec65d770c0633
0x0Louis <louis@joepegs.com>
2023-07-22 03:41:58 +0200

    add maker and taker fee

--- src/OrderFactory.sol
+++ src/OrderFactory.sol
@@ -2,17 +2,27 @@
 pragma solidity ^0.8.13;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract OrderFactory {
+contract OrderFactory is Ownable2Step {
     using SafeERC20 for IERC20;
 
     error InvalidMaker(address sender, address maker);
     error InvalidTaker(address sender, address taker);
     error InvalidState(State expected, State actual);
+    error InvalidTakerFee(uint48 takerFee);
+    error InvalidMakerFee(uint48 makerFee);
+    error InvalidFeeRecipient(address feeRecipient);
+    error SameTakerFee(uint48 takerFee);
+    error SameMakerFee(uint48 makerFee);
+    error SameFeeRecipient(address feeRecipient);
 
     event OrderCreated(uint256 indexed orderId, Party maker, Party taker);
     event OrderFilled(uint256 indexed orderId);
     event OrderCancelled(uint256 indexed orderId);
+    event TakerFeeSet(uint48 takerFee);
+    event MakerFeeSet(uint48 makerFee);
+    event FeeRecipientSet(address feeRecipient);
 
     enum State {
         Invalid,
@@ -33,12 +43,26 @@ contract OrderFactory {
         Party taker;
     }
 
+    uint256 public constant BASIS_POINTS = 10000;
+
     uint256 private _orderCount;
+
+    uint48 private _takerFee;
+    uint48 private _makerFee;
+    address private _feeRecipient;
 
     mapping(uint256 => Order) private _orders;
 
     function getOrder(uint256 orderId) external view returns (Order memory) {
         return _orders[orderId];
+    }
+
+    function getTakerFee() external view returns (uint48 takerFee) {
+        return _takerFee;
+    }
+
+    function getMakerFee() external view returns (uint48 makerFee) {
+        return _makerFee;
     }
 
     function createOrder(Party calldata maker, Party calldata taker) external {
@@ -67,8 +91,17 @@ contract OrderFactory {
 
         order.state = State.Filled;
 
-        order.taker.token.safeTransferFrom(msg.sender, address(this), order.taker.amount);
-        order.maker.token.safeTransfer(order.taker.account, order.maker.amount);
+        uint256 makerAmount = order.maker.amount;
+        uint256 takerAmount = order.taker.amount;
+
+        uint256 makerFee = (makerAmount * _makerFee) / BASIS_POINTS;
+        uint256 takerFee = (takerAmount * _takerFee) / BASIS_POINTS;
+
+        if (makerFee > 0) order.maker.token.safeTransfer(_feeRecipient, makerFee);
+        if (takerFee > 0) order.taker.token.safeTransfer(_feeRecipient, takerFee);
+
+        order.taker.token.safeTransferFrom(msg.sender, address(this), takerAmount - takerFee);
+        order.maker.token.safeTransfer(order.taker.account, makerAmount - makerFee);
 
         emit OrderFilled(orderId);
     }
@@ -85,5 +118,32 @@ contract OrderFactory {
         order.maker.token.safeTransfer(order.maker.account, order.maker.amount);
 
         emit OrderCancelled(orderId);
+    }
+
+    function setTakerFee(uint48 takerFee) external onlyOwner {
+        if (takerFee > BASIS_POINTS) revert InvalidTakerFee(takerFee);
+        if (takerFee == _takerFee) revert SameTakerFee(takerFee);
+
+        _takerFee = takerFee;
+
+        emit TakerFeeSet(takerFee);
+    }
+
+    function setMakerFee(uint48 makerFee) external onlyOwner {
+        if (makerFee > BASIS_POINTS) revert InvalidMakerFee(makerFee);
+        if (makerFee == _makerFee) revert SameMakerFee(makerFee);
+
+        _makerFee = makerFee;
+
+        emit MakerFeeSet(makerFee);
+    }
+
+    function setFeeRecipient(address feeRecipient) external onlyOwner {
+        if (feeRecipient == address(0)) revert InvalidFeeRecipient(feeRecipient);
+        if (feeRecipient == _feeRecipient) revert SameFeeRecipient(feeRecipient);
+
+        _feeRecipient = feeRecipient;
+
+        emit FeeRecipientSet(feeRecipient);
     }
 }
