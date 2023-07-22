9ca2be639f385216aaec8d3cddb97eb0bb7a7ac8
0x0Louis <louis@joepegs.com>
2023-07-22 03:33:41 +0200

    update create and add fill

--- src/OrderFactory.sol
+++ src/OrderFactory.sol
@@ -6,7 +6,11 @@ import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeE
 contract OrderFactory {
     using SafeERC20 for IERC20;
 
+    error InvalidMaker(address sender, address maker);
     error InvalidTaker(address sender, address taker);
+    error InvalidState(State expected, State actual);
+
+    event OrderCreated(uint256 indexed orderId, Party maker, Party taker);
 
     enum State {
         Invalid,
@@ -35,9 +39,35 @@ contract OrderFactory {
         return _orders[orderId];
     }
 
-    function createOrder(Order calldata order) external returns (uint256 orderId) {
+    function createOrder(Party calldata maker, Party calldata taker) external {
         orderId = _orderCount++;
 
-        _orders[orderId] = order;
+        if (maker.account != msg.sender) revert InvalidMaker(msg.sender, maker.account);
+
+        _orders[orderId] = Order({state: State.Created, maker: maker, taker: taker});
+
+        maker.token.safeTransferFrom(msg.sender, address(this), maker.amount);
+
+        emit OrderCreated(orderId, maker, taker);
+    }
+
+    function fillOrder(uint256 orderId) external {
+        Order storage order = _orders[orderId];
+
+        State state = order.state;
+
+        if (state != State.Created) revert InvalidState(State.Created, state);
+
+        address takerAccount = order.taker.account;
+
+        if (takerAccount == address(0)) order.taker.account = msg.sender;
+        else if (takerAccount != msg.sender) revert InvalidTaker(msg.sender, takerAccount);
+
+        order.state = State.Filled;
+
+        order.taker.token.safeTransferFrom(msg.sender, address(this), order.taker.amount);
+        order.maker.token.safeTransfer(order.taker.account, order.maker.amount);
+
+        emit OrderFilled(orderId);
     }
 }
