524f65fae72e3d917fecc09f71e2fc11dd42261b
0x0Louis <louis@joepegs.com>
2023-07-22 04:21:46 +0200

    add toAddress and address toBytes32

--- src/library/Bytes.sol
+++ src/library/Bytes.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 library Bytes {
     error InvalidLength(uint256 expected, uint256 actual);
+    error InvalidAddress();
 
     function toBytes32(bytes memory input) internal pure returns (bytes32 output) {
         if (input.length != 32) revert InvalidLength(32, input.length);
@@ -12,11 +13,25 @@ library Bytes {
         }
     }
 
+    function toBytes32(address input) internal pure returns (bytes32 output) {
+        assembly {
+            output := input
+        }
+    }
+
     function toBytes(bytes32 input) internal pure returns (bytes memory output) {
         output = new bytes(32);
 
         assembly {
             mstore(add(output, 0x20), input)
+        }
+    }
+
+    function toAddress(bytes32 input) internal pure returns (address output) {
+        if (uint256(input) > type(uint160).max) revert InvalidAddress();
+
+        assembly {
+            output := input
         }
     }
 }
