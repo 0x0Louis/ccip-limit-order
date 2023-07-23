b9fe42bde11ddd9206f815d5aa844d856c99e920
0x0Louis <louis@joepegs.com>
2023-07-23 02:26:42 +0200

    add wagmi boilerplate

---
+++ ccip-limit-order-fe/src/App.tsx
@@ -0,0 +1,26 @@
+import { useAccount } from "wagmi";
+
+import { Account } from "./components/Account";
+import { Connect } from "./components/Connect";
+import { ERC20 } from "./components/ERC20";
+import { NetworkSwitcher } from "./components/NetworkSwitcher";
+
+export function App() {
+  const { isConnected } = useAccount();
+
+  return (
+    <>
+      <h1>CCIP Limit Order</h1>
+
+      <Connect />
+
+      {isConnected && (
+        <>
+          <Account />
+          <ERC20 />
+          <NetworkSwitcher />
+        </>
+      )}
+    </>
+  );
+}
---
+++ ccip-limit-order-fe/src/wagmi.ts
@@ -0,0 +1,47 @@
+import { configureChains, createConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { avalancheFuji, sepolia } from "wagmi/chains";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
+import { publicProvider } from "wagmi/providers/public";
+
+const walletConnectProjectId = "51215d598d096b3100350fbdbeea0576";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [avalancheFuji, sepolia],
+  [
+    alchemyProvider({ apiKey: process.env.VITE_ALCHEMY_API_KEY }),
+    publicProvider(),
+  ]
+);
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi",
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: walletConnectProjectId,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+});
